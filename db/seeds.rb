74e283ba89c67181e51ba4501fdb1cdb163f201f
SophGits <sophie.jiffy@gmail.com>
2013-12-12 16:09:21 +0000

    more feeds>strips>comments functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,7 +28,7 @@ cyanide.save!
 cyanide_strip = Strip.create(strip_url: "http://www.explosm.net/db/files/Comics/Rob/hole2.png", keywords: "cyanide, individual strip", flag_count: 1, up_vote_count: 1, down_vote_count: 0, created_at: DateTime.now, feed_id: cyanide.id)
 cyanide_strip.save!
 
-comment1 = cyanide_strip.comments.create(content: "Hey, I love this one!", flag_count: 0, up_vote_count: 2, down_vote_count: 1)
+comment1 = cyanide_strip.comments.create(content: "Hey, I love this one!", flag_count: 0, up_vote_count: 2, down_vote_count: 1, user_id: bob.id)
 comment1.save!
 
 dilbert = Feed.create(title: "Dilbert, yo", logo: "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-ash1/373035_163128967074041_832978965_q.jpg", main_url: "http://www.dilbert.com", feed_url: "http://feed.dilbert.com/dilbert/daily_strip", keywords: "dilbert, dogbert etc", summary: "You know: Dilbert...", author: "Scott Adams", flag_count: 0, up_vote_count: 15, down_vote_count: 1, created_at: DateTime.now)
@@ -37,5 +37,5 @@ dilbert.save!
 dilbert_strip = Strip.create(strip_url: "http://www.dilbert.com/dyn/str_strip/000000000/00000000/0000000/200000/00000/6000/400/206400/206400.strip.gif", keywords: "Dilbert, Dogbert, the cat from HR etc", up_vote_count: 5, down_vote_count: 0, feed_id: dilbert.id)
 dilbert_strip.save!
 
-comment2 = dilbert_strip.comments.create(content: "Hey! Everybody loves Dogbert, right?!", flag_count: 0, up_vote_count: 2, down_vote_count: 1)
+comment2 = dilbert_strip.comments.create(content: "Hey! Everybody loves Dogbert, right?!", flag_count: 0, up_vote_count: 2, down_vote_count: 1, user_id: shaniqua.id)
 comment1.save!

467a8f12ee7b219ffaa247811564175ff832ff61
SophGits <sophie.jiffy@gmail.com>
2013-12-14 21:17:33 +0000

    Tried to prevent empty messages being sent with validation in model, but nothing doing

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,7 @@
 class Message < ActiveRecord::Base
   attr_accessible :content, :is_read, :recipient_id, :sender_id
   attr_accessor :recipient_username, :sender_username
+
+   validates :content, presence: true #doesn't do much
+
 end
